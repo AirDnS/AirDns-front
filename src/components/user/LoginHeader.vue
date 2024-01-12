f1164047115f570c25c2cfe0330568db2575c624
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-12 15:06:33 +0900

    Feat : "로그인 페이지 틀 구현"
    - 로그인 페이지 CSS, HTML 작업 완료
    
    Resolves: #2

---
+++ src/components/user/LoginHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar fixed>
+    <v-icon class="v-icon">mdi-desk</v-icon>
+    <v-app-bar-title>airDnS</v-app-bar-title>
+  </v-app-bar>
+</template>
+<script>
+import router from "@/routers";
+
+export default {
+  methods: {
+    goLogin() {
+      router.push('login')
+    }
+  }
+}
+</script>
+
+<style>
+.login-btn {
+  margin-right: 24px;
+}
+.v-icon {
+  padding-left: 20px;
+}
+
+</style>
