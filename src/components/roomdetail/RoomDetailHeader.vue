3197a7ffc71b410c34dc2c5f4d648785f4e0fd81
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2024-01-25 00:29:37 +0900

    refactor: "전체 구조 변경"
    navigator 추가를 위해 화면 구조 변경

--- src/components/roomdetail/RoomDetailHeader.vue
+++ src/components/roomdetail/RoomDetailHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar fixed>
-    <v-icon class="v-icon">mdi-desk</v-icon>
+    <v-icon class="v-icon airdns-logo">mdi-desk</v-icon>
     <v-app-bar-title v-on:click="goHome">airDnS</v-app-bar-title>
   </v-app-bar>
 </template>
