7567423eb343aec49de25cae04053de8d521e07d
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-27 22:52:40 +0900

    Refactor: "로그인 로그앗 UI 수정"
    Related to: #101

--- src/components/Logout/LogoutRedirect.vue
+++ src/components/Logout/LogoutRedirect.vue
@@ -9,10 +9,7 @@ import router from "@/routers";
 
 export default {
   created() {
-    localStorage.removeItem("accessToken");
-    if (this.$cookies.get("Authorization")) {
-      this.$cookies.remove("Authorization");
-    }
+    localStorage.removeItem("userInfo");
     window.alert('로그아웃에 성공했습니다')
     router.push({name: 'HomePage'})
   }
--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -7,16 +7,16 @@
       </div>
     </RouterLink>
     <v-spacer></v-spacer>
-    <v-btn variant="outlined" class = "login-btn" v-on:click="goCreateRoom" v-show="isHost">
+    <v-btn variant="outlined" class="login-btn" v-on:click="goCreateRoom">
       방등록
     </v-btn>
-    <v-btn variant="outlined" class="userDetail-btn" v-on:click="goUserDetail" v-show="hasUser">
+    <v-btn variant="outlined" class="userDetail-btn" v-on:click="goUserDetail">
       회원 정보
     </v-btn>
-    <v-btn variant="outlined" class="logout-btn" v-on:click="goLogout" v-show="hasUser">
+    <v-btn variant="outlined" class="logout-btn" v-on:click="goLogout">
       Logout
     </v-btn>
-    <v-btn variant="outlined" class="login-btn" v-on:click="goLogin" v-show="!hasUser">
+    <v-btn variant="outlined" class="login-btn" v-on:click="goLogin">
       Login
     </v-btn>
   </v-app-bar>
@@ -27,31 +27,35 @@ import router from "@/routers";
 export default {
   data() {
     return {
-      hasUser: false,
-      isHost: false,
+      userInfo: {}
     }
   },
-  mounted() {
-    if (localStorage.getItem('accessToken') != null) {
-      this.hasUser = true;
-      console.log(JSON.parse(localStorage.getItem('userInfo'))["role"]);
-      if (JSON.parse(localStorage.getItem('userInfo'))["role"] === "HOST") {
-        this.isHost = true;
-      }
-    }
-  },
+  // 보안 상 Cookie JavaScript를 통해 관리를 하는 것은 안좋다 -> 쿠키 탈취문제(cross-site scripting XSS)
+  // UserInfo를 이용한다.
   methods: {
     goLogin() {
       router.push('login')
     },
     goLogout() {
+      if (localStorage.getItem("userInfo") != null) {
         window.location.href = process.env.VUE_APP_API_URL + "/oauth2/authorization/kakao?redirect_uri=" + process.env.VUE_APP_URL + "/unlink/oauth2/redirect&mode=unlink";
+      } else {
+        window.alert("로그인을 해야합니다!!!")
+      }
     },
     goUserDetail() {
-      router.push('userdetail')
+      if (localStorage.getItem("userInfo") != null) {
+        router.push('userdetail')
+      } else {
+        window.alert("로그인을 해야합니다!!!")
+      }
     },
     goCreateRoom() {
-      router.push({name:'CreateRoom'})
+      if (JSON.parse(localStorage.getItem("userInfo")).role === "HOST") {
+        router.push({name: 'CreateRoom'})
+      } else {
+        window.alert("권한이 없습니다.")
+      }
     }
   }
 }
