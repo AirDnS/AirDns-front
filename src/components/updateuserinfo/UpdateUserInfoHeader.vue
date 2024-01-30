c387806e7a3f88ac59201dfc1042d8178af6417b
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2024-01-30 23:47:43 +0900

    Fix: "해더 버그 수정"
    유저 수정 페이지에서 상단 헤더 클릭시 아무런 반응 없던 현상 수정

--- src/components/updateuserinfo/UpdateUserInfoHeader.vue
+++ src/components/updateuserinfo/UpdateUserInfoHeader.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app-bar fixed>
-    <v-icon class="v-icon">mdi-desk</v-icon>
-    <v-app-bar-title>airDnS</v-app-bar-title>
+    <RouterLink to="/" style="text-decoration: none; color: inherit;">
+      <div class="airdns-logo">
+        <v-icon class="v-icon">mdi-desk</v-icon>
+        <v-app-bar-title>airDnS</v-app-bar-title>
+      </div>
+    </RouterLink>
   </v-app-bar>
 </template>
 <script>
