d72d4145bae9735449d1ce58137ee5a619f3182a
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2024-01-30 23:41:12 +0900

    Feat: "방 등록 validation 추가"
    방 등록에 대한 validtaion 추가

--- src/components/room/RoomCreate.vue
+++ src/components/room/RoomCreate.vue
@@ -1,104 +1,117 @@
 <template>
   <v-sheet class="pa-12" rounded>
-    <v-card class="mx-auto px-6 py-8" max-width="540px">
-        <v-text-field
-            v-model="data.name"
-            label="방 이름"
-            variant="outlined"
-        ></v-text-field>
-        <v-textarea
-            v-model="data.desc"
-            label="방 상세설명"
-            auto-grow
-            variant="outlined"
-            row-height="25"
-            shaped
-        ></v-textarea>
-        <div class="postcodeBox">
-          <v-text-field type="text" v-model="data.address" placeholder="주소" readonly></v-text-field>
-          <v-btn class="post-btn" id="postcode" @click="openPostcode">검색
-          </v-btn>
-        </div>
-        <v-text-field
-            v-model="addressDetail"
-            label="상세 주소 "
-            variant="outlined"
-            placeholder="상세주소를 입력해주세요"
-        ></v-text-field>
-        <v-text-field
-            v-model="data.price"
-            label="방 가격"
-            variant="outlined"
-            placeholder="시간당 가격 기준으로 입력해주세요"
-        ></v-text-field>
-        <v-text-field
-            v-model="data.size"
-            label="방 평수"
-            variant="outlined"
-            placeholder="평수 기준으로 입력해주세요"
-        ></v-text-field>
-        <!-- <div class="output">Data: {{ data.equipment }}</div> -->
-        <Multiselect
-            v-model="data.equipment"
-            v-bind="example12"
-        ></Multiselect>
-        <br>
-        <v-file-input
-            label="File input"
-            variant="outlined"
-            multiple
-            v-model="files"
-            type="file"
-            accept="image/*"
-            @change="previewImage">
-          >
-          <template v-slot:selection="{ fileNames }">
-            <template v-for="(fileName, index) in fileNames" :key="fileName">
-              <v-chip
-                  v-if="index < 2"
-                  color="deep-purple-accent-4"
-                  label
-                  size="small"
-                  class="me-2"
-              >
-                {{ fileName }}
-              </v-chip>
-              <span
-                  v-else-if="index === 2"
-                  class="text-overline text-grey-darken-3 mx-2">
-              +{{ files.length - 2 }} 파일
-              </span>
-            </template>
-
-          </template>
-        </v-file-input>
-        <v-row>
-          <template v-for="v in files" :key="v">
-            <v-col>
-              <v-img :src="previewImage(v)" width="500px" height="280px" class="room-image"></v-img>
-            </v-col>
-          </template>
-        </v-row>
-        <br>
-        <div class="btn-box">
-          <div>
-            <v-btn
-                class="create-btn"
-                color="primary"
-                v-on:click="postCreateRoom"
-                variant="elevated">
-              등록하기
-            </v-btn>
-            <v-btn
-                class="home-btn"
-                v-on:click="goHome"
-                color="error"
-                variant="elevated">
-              취소하기
+    <v-form ref="form">
+      <v-card class="mx-auto px-6 py-8" max-width="540px">
+          <v-text-field
+              :rules="rules.name"
+              v-model="data.name"
+              label="방 이름"
+              variant="outlined"
+          ></v-text-field>
+          <v-textarea
+              :rules="[v =>  !!v || '설명은 필수 입력입니다.']"
+              v-model="data.desc"
+              label="방 상세설명"
+              auto-grow
+              variant="outlined"
+              row-height="25"
+              shaped
+          ></v-textarea>
+          <div class="postcodeBox">
+            <v-text-field
+              :rules="[v =>  !!v || '주소를 입력해주세요.']"
+              type="text"
+              v-model="data.address"
+              placeholder="주소"
+              readonly></v-text-field>
+            <v-btn class="post-btn" id="postcode" @click="openPostcode">검색
             </v-btn>
           </div>
-        </div>
-    </v-card>
+          <v-text-field
+              :rules="[v =>  !!v || '상세 주소를 입력해주세요.']"
+              v-model="addressDetail"
+              label="상세 주소 "
+              variant="outlined"
+              placeholder="상세주소를 입력해주세요"
+          ></v-text-field>
+          <v-text-field
+              :rules="rules.price"
+              v-model="data.price"
+              label="방 가격"
+              variant="outlined"
+              placeholder="시간당 가격 기준으로 입력해주세요"
+          ></v-text-field>
+          <v-text-field
+              :rules="rules.size"
+              v-model="data.size"
+              label="방 평수"
+              variant="outlined"
+              placeholder="평수 기준으로 입력해주세요"
+          ></v-text-field>
+          <!-- <div class="output">Data: {{ data.equipment }}</div> -->
+          <Multiselect
+              v-model="data.equipment"
+              v-bind="example12"
+          ></Multiselect>
+          <br>
+          <v-file-input
+              :rules="rules.files"
+              label="File input"
+              variant="outlined"
+              multiple
+              v-model="files"
+              type="file"
+              accept="image/*"
+              @change="previewImage">
+            >
+            <template v-slot:selection="{ fileNames }">
+              <template v-for="(fileName, index) in fileNames" :key="fileName">
+                <v-chip
+                    v-if="index < 2"
+                    color="deep-purple-accent-4"
+                    label
+                    size="small"
+                    class="me-2"
+                >
+                  {{ fileName }}
+                </v-chip>
+                <span
+                    v-else-if="index === 2"
+                    class="text-overline text-grey-darken-3 mx-2">
+                +{{ files.length - 2 }} 파일
+                </span>
+              </template>
+
+            </template>
+          </v-file-input>
+          <v-row>
+            <template v-for="v in files" :key="v">
+              <v-col>
+                <v-img :src="previewImage(v)" width="500px" height="280px" class="room-image"></v-img>
+              </v-col>
+            </template>
+          </v-row>
+          <br>
+          <div class="btn-box">
+            <div>
+              <v-btn
+                  class="create-btn"
+                  color="primary"
+                  v-on:click="postCreateRoom"
+                  variant="elevated">
+                등록하기
+              </v-btn>
+              <v-btn
+                  class="home-btn"
+                  v-on:click="goHome"
+                  color="error"
+                  variant="elevated">
+                취소하기
+              </v-btn>
+            </div>
+          </div>
+      </v-card>
+    </v-form>
   </v-sheet>
 </template>
 
@@ -113,8 +126,42 @@ export default {
   },
   data: function () {
     return {
-      accessToken: {
-        accessToken: ""
+      rules: {
+        name: [
+          v => !!v || '방 이름을 입력해주세요.',
+          v => (v && v.length >= 4) || '최소 4자 이상이어야 합니다.',
+        ],
+        price: [
+          v => !!v || '가격을 입력해주세요.',
+          v => {
+            const pattern = /^[0-9]+$/
+            return pattern.test(v) || "숫자만 입력 가능합니다."
+          }
+        ],
+        size: [
+          v => !!v || '평수를 입력해주세요.',
+          v => {
+            const pattern = /^[0-9]+$/
+            return pattern.test(v) || "숫자만 입력 가능합니다."
+          }
+        ],
+        files: [
+          v => !v || !!v.length || '방 사진을 업로드해주세요.',
+          v => {
+            for (let file of v) {
+              console.log(file);
+              if (file.size > 200_0000)
+                return '사진은 2MB 이하 크기만 업로드 가능합니다.'
+            }
+            return true;
+          },
+
+        ]
+
+        // phone: value => {
+        //   const pattern = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/
+        //   return pattern.test(value) || "잘못된 핸드폰 번호 입력입니다."
+        // }
       },
       addressDetail: "",
       data: {
@@ -149,7 +196,11 @@ export default {
       this.example12.options = JSON.parse(localStorage.getItem("equipment")).options;
       console.log(JSON.parse(localStorage.getItem("equipment")).options);
     },
-    postCreateRoom: function () {
+    postCreateRoom: async function () {
+      const { valid } = await this.$refs.form.validate();
+      if (!valid)
+        return;
+
       const frm = new FormData();
       this.data.address = this.data.address + " " + this.addressDetail
       const json = JSON.stringify(this.data);
@@ -205,7 +256,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.v-text-field {
+  margin-bottom: 7px;
+}
 .postcodeBox {
   display: flex;
 }
